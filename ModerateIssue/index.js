273c6b36a4626ae62fd034267102f9b34a59b984
Pierluigi Cau <pierluigi@github.com>
2018-09-20 16:30:34 +0100

    Use ghapplets

--- ModerateIssue/index.js
+++ ModerateIssue/index.js
@@ -1,13 +1,15 @@
-const octokit = require("@octokit/rest");
-const jsonwebtoken = require("jsonwebtoken");
-
 // Set these in your Azure Function Application Settings
 const appId = process.env["APP_ID"];
 const installationId = process.env["APP_INST_ID"];
 const pem = process.env["APP_PEM"];
 
+const octokit = require("@octokit/rest");
+const jsonwebtoken = require("jsonwebtoken");
+
 function generateJwtToken() {
   // Sign with RSA SHA256
+  console.log(`appId = ${appId}`);
+  console.log(`pem = ${pem}`);
   return jsonwebtoken.sign(
     {
       iat: Math.floor(new Date() / 1000),
@@ -19,87 +21,70 @@ function generateJwtToken() {
   );
 }
 
-// function postIssueComment(
-//   installationId,
-//   owner,
-//   repository,
-//   number,
-//   action
-// ) {
-//   console.log(`generate jwt token`);
-//   await octokit.authenticate({
-//     type: "app",
-//     token: generateJwtToken()
-//   });
-
-//   console.log(`generate installation token ${installationId}`);
-//   const {
-//     data: { token }
-//   } = await octokit.apps.createInstallationToken({
-//     installation_id: installationId
-//   });
-
-//   console.log(`authenticate with token`);
-//   octokit.authenticate({ type: "token", token });
-
-//   var result = await octokit.issues.createComment({
-//     owner,
-//     repo: repository,
-//     number,
-//     body: `Updated function for Azure Functions demo for action ${action} and appId ${appId}.`
-//   });
-//   return result;
-// }
-
-module.exports = function(context, data) {
-  context.log("GitHub Webhook triggered!", data.comment.body);
-  context.log("GitHub IDs (app, installation):", appId, installationId);
-  context.res = { body: "New GitHub comment: " + data.comment.body };
-
-  // Synchronous operation
-  octokit.authenticate({
+async function postIssueComment(
+  installationId,
+  owner,
+  repository,
+  number,
+  action
+) {
+  console.log(`generate jwt token`);
+  await octokit.authenticate({
     type: "app",
     token: generateJwtToken()
   });
 
-  octokit.apps
-    .createInstallationToken({
-      installation_id: installationId
-    })
-    .then(function(data) {
-      context.log("createInstallationToken called with", data);
-      octokit.authenticate({ type: "token", token: data.token });
+  console.log(`generate installation token ${installationId}`);
+  const {
+    data: { token }
+  } = await octokit.apps.createInstallationToken({
+    installation_id: installationId
+  });
 
-      // End
-      context.res = {
-        body: { text: "Response here" },
-        headers: { "Content-Type": "application/json" }
-      };
+  console.log(`authenticate with token`);
+  octokit.authenticate({ type: "token", token });
 
-      context.done();
-    })
-    .catch(function(e) {
-      context.error("Error createInstallationToken: ", e);
-    });
+  var result = await octokit.issues.createComment({
+    owner,
+    repo: repository,
+    number,
+    body: `Updated Function for Kubernetes demo for action ${action} and appId ${appId}.`
+  });
+  return result;
+}
 
-  /*
-  const stringBody = JSON.stringify(data);
+module.exports = async function(context) {
+  const stringBody = JSON.stringify(context.request.body);
   const body = JSON.parse(stringBody);
-
   const action = body.action;
   const number = body.issue.number;
   const repository = body.repository.name;
   const owner = body.repository.owner.login;
 
-  var response = "";
-  if (action === "opened") {
-    response = await postIssueComment(
-      installationId,
-      owner,
-      repository,
-      number,
-      action
-    );
+  try {
+    var response = "";
+    if (action === "opened") {
+      response = await postIssueComment(
+        installationId,
+        owner,
+        repository,
+        number,
+        action
+      );
+    }
+    return {
+      status: 200,
+      body: {
+        text: response
+      },
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+  } catch (e) {
+    return {
+      status: 500,
+      body: e
+    };
   }
-  */
 };
---
+++ test/moderateIssueTest.js
@@ -0,0 +1,13 @@
+var assert = require("assert");
+
+describe("ModerateIssue", function() {
+  before(function() {
+    moderateIssue = require("../ModerateIssue/index");
+  });
+
+  describe("moderateIssue", function() {
+    it("should OK", function() {
+      assert.ok(true);
+    });
+  });
+});
