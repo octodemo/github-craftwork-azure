6a6a72aecb6422713dbdbd7a7ac16c03869823c3
Pierluigi Cau <pierluigi@github.com>
2018-09-24 17:21:37 +0100

    Better log

--- Testing/index.js
+++ Testing/index.js
@@ -5,20 +5,10 @@ const pem = process.env["APP_PEM"];
 
 const octokit = require("@octokit/rest");
 const jsonwebtoken = require("jsonwebtoken");
-module.exports = function(context, req) {
-  context.log("JavaScript HTTP trigger function processed a request.");
-  context.log("Node version:", process.version);
 
-  if (req.query.name || (req.body && req.body.name)) {
-    context.res = {
-      // status: 200, /* Defaults to 200 */
-      body: "Hello " + (req.query.name || req.body.name)
-    };
-  } else {
-    context.res = {
-      status: 400,
-      body: "Please pass a name on the query string or in the request body"
-    };
-  }
+module.exports = function(context, data) {
+  context.log("Node version:", process.version);
+  context.log("GitHub Webhook triggered!", data.comment.body);
+  context.res = { body: "New GitHub comment: " + data.comment.body };
   context.done();
 };
