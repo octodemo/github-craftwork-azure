4c7a71d08cb64a74fd939ef59bb036361bb733bc
Sebass van Boxel <svboxel@gmail.com>
2018-10-10 11:20:09 +0200

    Moved functions to separate files

---
+++ ModerateIssue/authenticate.js
@@ -0,0 +1,33 @@
+const appId = process.env.APP_ID
+
+const pem = Buffer.from(process.env["APP_PEM"] || "", "base64").toString();
+
+const octokit = require("@octokit/rest")();
+const jsonwebtoken = require("jsonwebtoken");
+
+// Sign with RSA SHA256
+function generateJwtToken() {
+  return jsonwebtoken.sign(
+    {
+      iat: Math.floor(new Date() / 1000),
+      exp: Math.floor(new Date() / 1000) + 60,
+      iss: appId
+    },
+    pem,
+    { algorithm: "RS256" }
+  );
+}
+
+module.exports = async function authenticate(installation_id) {
+  // Create bearer token and initial authentication session
+  await octokit.authenticate({
+    type: "app",
+    token: generateJwtToken()
+  });
+
+  // Retrieve token from app installation
+  const { data: { token } } = await octokit.apps.createInstallationToken({ installation_id });
+
+  // Finally authenticate as the app
+  return octokit.authenticate({ type: "token", token });
+}
--- ModerateIssue/index.js
+++ ModerateIssue/index.js
@@ -1,93 +1,26 @@
-// Set these in your Azure Function Application Settings
-const appId = process.env.APP_ID
-const luisAppId = process.env.LUIS_APP_ID;
-const endpointKey = process.env.LUIS_ENDPOINT_KEY;
-
-// Decode our secret pem file
-const pem = Buffer.from(process.env["APP_PEM"] || "", "base64").toString();
-
 const octokit = require("@octokit/rest")();
-const jsonwebtoken = require("jsonwebtoken");
-const request = require('request');
-const querystring = require('querystring');
-
-const luisEndpoint = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/";
-
-// Sign with RSA SHA256
-function generateJwtToken() {
-  return jsonwebtoken.sign(
-    {
-      iat: Math.floor(new Date() / 1000),
-      exp: Math.floor(new Date() / 1000) + 60,
-      iss: appId
-    },
-    pem,
-    { algorithm: "RS256" }
-  );
-}
-
-async function authenticate(installation_id) {
-  // Create bearer token and initial authentication session
-  await octokit.authenticate({
-    type: "app",
-    token: generateJwtToken()
-  });
-
-  // Retrieve token from app installation
-  const { data: { token } } = await octokit.apps.createInstallationToken({ installation_id });
-
-  // Finally authenticate as the app
-  return octokit.authenticate({ type: "token", token });
-}
-
-function getLuisIntent(utterance) {
-  const queryParams = {
-    "verbose": true,
-    "q": utterance,
-    "subscription-key": endpointKey
-  }
-
-  const luisRequest = `${luisEndpoint}/${luisAppId}?${querystring.stringify(queryParams)}`;
-
-  return new Promise((resolve, reject) => {
-    request(luisRequest, (err, _, body) => {
-        if (err) reject(err)
-        
-        const data = JSON.parse(body);
-        resolve(data.topScoringIntent.intent.toLowerCase());
-      })
-  });
-}
-
-async function addLabelsFromTitle(owner, repo, number, title) {
-  const label = await getLuisIntent(title);
-
-  return await octokit.issues.addLabels({
-    owner,
-    repo,
-    number,
-    labels: [label]
-  });
-}
+const authenticate = require('./authenticate');
+const getLuisIntent = require('./authenticate');
 
 module.exports = async function (context, data) {
   const { body } = data
   const { action, repository, issue, installation } = body;
   const { number, title } = issue;
-  const repositoryName = repository.name;
-  const repositoryOwner = repository.owner.login;
+  const repo = repository.name;
+  const owner = repository.owner.login;
   const installationId = installation.id;
 
   try {
     let response = "";
     if (action === "opened") {
       await authenticate(installationId)
-      response = await addLabelsFromTitle(
-        repositoryOwner,
-        repositoryName,
+      const label = await getLuisIntent(title);
+      response = await octokit.issues.addLabels({
+        owner,
+        repo,
         number,
-        title
-      );
+        labels: [label]
+      });
     }
     context.res = {
       status: 200,
