41cdb636892a7d07bcdb96118b17a98931c5dc28
Pierluigi Cau <pierluigi@github.com>
2018-11-14 16:48:01 +0000

    Fix tests

--- test/moderateIssueTest.js
+++ test/moderateIssueTest.js
@@ -1,36 +1,36 @@
 var sinon = require("sinon");
-var proxyquire =  require('proxyquire')
-var issueOpenedPayload = require('./fixtures/issue-opened.json')
+var proxyquire = require("proxyquire");
+var issueOpenedPayload = require("./fixtures/issue-opened.json");
 
 describe("ModerateIssue", function() {
-  var moderateIssue
+  var moderateIssue;
 
   before(function() {
-    createCommentStub = sinon.spy()
-    addLabelsStub = sinon.spy()
+    createCommentStub = sinon.spy();
+    addLabelsStub = sinon.spy();
     octoKitStubs = {
       issues: {
         addLabelsStub: addLabelsStub,
         createComment: createCommentStub
       }
-    }
-
+    };
 
     moderateIssue = proxyquire("../ModerateIssue/index", {
-      '@octokit/rest': () => octoKitStubs,
-      './authenticate': () => true,
-      './get-luis-intent': () => 'question'
+      "@octokit/rest": () => octoKitStubs,
+      "./authenticate": () => true,
+      "./get-luis-intent": () => "question"
     });
   });
 
-  it('should create comment when issue is opened', async () => {
-    await moderateIssue({}, { body: issueOpenedPayload })
+  it("should create comment when issue is opened", async () => {
+    await moderateIssue({}, { body: issueOpenedPayload });
 
     sinon.assert.calledWith(createCommentStub, {
-      body: "Thanks for submitting this issue. We will take a look at it!",
+      body:
+        "Thanks for submitting this issue. We will take a look at it later!",
       number: issueOpenedPayload.issue.number,
       owner: issueOpenedPayload.repository.owner.login,
       repo: issueOpenedPayload.repository.name
     });
-  })
+  });
 });
